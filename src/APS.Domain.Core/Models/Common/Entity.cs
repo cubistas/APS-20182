cbc11c3a47cb4fa2940e4d0a0955013284b1e439
rcoliveira2016 <roramon7@gmail.com>
2018-03-19 01:29:55 -0300

    Initial commit

---
+++ src/APS.Domain.Core/Models/Common/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APS.Domain.Core.Models.Common
+{
+    public class Entity
+    {
+        public long Id { get; set; }
+
+        public bool IsNew { get => Id <= 0; }
+    }
+}
---
+++ src/APS.Presentation.Web/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace APS.Presentation.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult InternalServerError()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}

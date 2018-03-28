bb161d65a1b747ac2c0ce24d2a944d6f54012e75
cubacubista <trabalhofsgbabok@gmail.com>
2018-03-27 22:12:33 -0300

    negocio para sessao

---
+++ src/APS.Infra.CrossCutting.Session/ControleSessao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace APS.Infra.CrossCutting.Session
+{
+
+    public static class ControleSessao
+    {
+        public static T ObterItemSessao<T>(string chave)
+        {
+            return (T)HttpContext.Current.Session[chave];
+        }               
+
+        public static void SetarItemSessao<T>(T value, string chave)
+        {
+            HttpContext.Current.Session[chave] = value;
+        }
+    }
+    
+}

b03a4b9bf8cd5105200c3a6aca1edd136da635bf
cubacubista <trabalhofsgbabok@gmail.com>
2018-06-11 22:08:05 -0300

    img helper
    img helper

---
+++ src/APS.Presentation.Web/Helpers/HTML/ImagemReder/ImageReder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APS.Presentation.Web.Helpers.HTML.ImagemReder
+{
+    public static class ImageRederHelpér
+    {
+        public static MvcHtmlString ImageReder(this HtmlHelper helper, string base64, object atributes=null)
+        {
+            return MvcHtmlString.Create(ImgHtml(base64, atributes));
+        }
+
+
+        private static string ImgHtml(string base64, object atributes)
+        {
+            var builder = new TagBuilder("img");
+
+            if (atributes != null)
+            {
+                var htmlAttributes = HtmlHelper.AnonymousObjectToHtmlAttributes(atributes);
+                builder.MergeAttributes(htmlAttributes);
+            }            
+
+            builder.Attributes.Add("src", $"data:image/png;base64, {base64}");
+
+            return builder.ToString(TagRenderMode.Normal);
+        }
+
+    }
+
+}
